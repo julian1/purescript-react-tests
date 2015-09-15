0d4fd188b18705e85c231b5761ae37574b338341
julian1 <julian.atkinson71@gmail.com>
2015-09-15 12:07:56 +0000

    update

---
+++ src/js/components/Login.js
@@ -0,0 +1,17 @@
+
+// import React from 'react';
+var React = require('react');
+
+let Login = React.createClass({ 
+
+  render:function(){
+   // return (
+  //render() {
+    return( <div>Welcome to login</div>);
+  }
+});
+
+// export default Login;  
+
+module.exports = Login;
+
