3ba9afbc3d629b7e56d7a91c9d7a00e98f833d93
julian1 <julian.atkinson71@gmail.com>
2015-09-16 20:19:20 +1000

    update

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var source = require("vinyl-source-stream");
 // need to modify...
 
 // https://truongtx.me/2014/07/18/using-reactjs-with-browserify-and-gulp/
+// https://viget.com/extend/gulp-browserify-starter-faq
+// http://christianalfoni.github.io/javascript/2014/08/15/react-js-workflow.html
 
 gulp.task('browserify', function(){
   var b = browserify();
@@ -25,6 +27,6 @@ gulp.task('copy', function() {
 
 gulp.task('default',['browserify', 'copy']);
 
-gulp.task('watch', function() {
-    gulp.watch('src/**/*.*', ['default']);
-});
+//gulp.task('watch', function() {
+//    gulp.watch('src/**/*.*', ['default']);
+//});
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -3,5 +3,8 @@ var React = require('react');
 
 var view = require('./view.jsx'); // need to specify the jsx extension
 
-React.renderComponent(view(), document.getElementById('content'));
+//React.renderComponent(view(), document.getElementById('content'));
+
+
+React.renderComponent(<view/>, document.getElementById('content') );
 
